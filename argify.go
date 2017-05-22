30243e57bbc4327ab2354f53e3be78f43383607f
dc0d <kaveh.shahbazian@gmail.com>
2017-05-22 13:04:57 +0430

    initial commit

---
+++ argify.go
@@ -0,0 +1,218 @@
+package argify
+
+import (
+	"fmt"
+	"reflect"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/dc0d/inflect"
+	"github.com/dc0d/inflect/inflectlab"
+	"github.com/urfave/cli"
+)
+
+// Argify adds arguments based on fields of a struct and binds them too
+type Argify struct {
+	appName string
+}
+
+// NewArgify creates an instance of Argify
+func NewArgify() *Argify {
+	return &Argify{}
+}
+
+func (b *Argify) extractSharedParts(
+	commandName string,
+	k1 string,
+	v1 inflectlab.Field) (name, usage, envVar string, hidden bool) {
+	name = strings.ToLower(k1)
+	if v, ok := v1.Tags["name"]; ok {
+		name = v
+	}
+	usage = ""
+	if v, ok := v1.Tags["usage"]; ok {
+		usage = v
+	}
+	envVar = ""
+	if v, ok := v1.Tags["envvar"]; ok {
+		envVar = v
+	}
+	if envVar == "" {
+		envName := name
+		envParts := strings.Split(envName, ",")
+		if len(envParts) > 1 {
+			envName = envParts[0]
+			if len(envParts[1]) > len(envName) {
+				envName = envParts[1]
+			}
+		}
+		if commandName == "" {
+			envVar = strings.Join([]string{b.appName, envName}, "_")
+		} else {
+			envVar = strings.Join([]string{b.appName, commandName, envName}, "_")
+		}
+		envVar = strings.Trim(envVar, "_")
+		envVar = strings.ToUpper(envVar)
+	}
+	hidden = false
+	if v, ok := v1.Tags["hidden"]; ok {
+		if b, err := strconv.ParseBool(v); err == nil {
+			hidden = b
+		}
+	}
+
+	return
+}
+
+func (b *Argify) setSharedParts(f cli.Flag, name, usage, envVar string, hidden bool) {
+	if err := inflect.Set(f, "Name", name); err != nil {
+		// TODO:
+	}
+	if err := inflect.Set(f, "Usage", usage); err != nil {
+		// TODO:
+	}
+	if err := inflect.Set(f, "EnvVar", envVar); err != nil {
+		// TODO:
+	}
+	if err := inflect.Set(f, "Hidden", hidden); err != nil {
+		// TODO:
+	}
+}
+
+func (b *Argify) process(
+	commandName string,
+	fields map[string]inflectlab.Field,
+	commands *[]cli.Command,
+	flags *[]cli.Flag) {
+	var keys []string
+	for k1 := range fields {
+		keys = append(keys, k1)
+	}
+	sort.Strings(keys)
+
+NEXT_FIELD:
+	for _, k1 := range keys {
+		v1 := fields[k1]
+		for kcmd := range *commands {
+			if strings.EqualFold((*commands)[kcmd].Name, k1) {
+				var subCommands []cli.Command = (*commands)[kcmd].Subcommands
+				var cmdFlags = (*commands)[kcmd].Flags
+				if cmdFlags == nil {
+					cmdFlags = []cli.Flag{}
+				}
+				b.process((*commands)[kcmd].Name, v1.Children, &subCommands, &cmdFlags)
+				(*commands)[kcmd].Subcommands = subCommands
+				(*commands)[kcmd].Flags = cmdFlags
+
+				continue NEXT_FIELD
+			}
+		}
+
+		var f cli.Flag
+
+		//
+
+		defaultValue := reflect.Zero(v1.Ptr.Type()).Interface()
+		var tagString string
+		if v, ok := v1.Tags["value"]; ok && v != fmt.Sprintf("%v", defaultValue) {
+			tagString = v
+		}
+		var tagValue interface{}
+
+		switch v1.Ptr.Type() {
+		case reflect.TypeOf(true):
+			f = &cli.BoolFlag{}
+			if tagString != "" {
+				if vp, err := strconv.ParseBool(tagString); err == nil {
+					tagValue = vp
+				}
+			}
+		case reflect.TypeOf(""):
+			f = &cli.StringFlag{}
+			if tagString != "" {
+				tagValue = tagString
+			}
+		case reflect.TypeOf(time.Second):
+			f = &cli.DurationFlag{}
+			if tagString != "" {
+				if vp, err := time.ParseDuration(tagString); err == nil {
+					tagValue = vp
+				}
+			}
+		case reflect.TypeOf(float64(1.0)):
+			f = &cli.Float64Flag{}
+			if tagString != "" {
+				if vp, err := strconv.ParseFloat(tagString, 64); err == nil {
+					tagValue = vp
+				}
+			}
+		case reflect.TypeOf(int64(1)):
+			f = &cli.Int64Flag{}
+			if tagString != "" {
+				if vp, err := strconv.ParseInt(tagString, 10, 64); err == nil {
+					tagValue = vp
+				}
+			}
+		case reflect.TypeOf(int(1)):
+			f = &cli.IntFlag{}
+			if tagString != "" {
+				if vp, err := strconv.ParseInt(tagString, 10, 64); err == nil {
+					tagValue = int(vp)
+				}
+			}
+		case reflect.TypeOf(uint(1)):
+			f = &cli.UintFlag{}
+			if tagString != "" {
+				if vp, err := strconv.ParseInt(tagString, 10, 64); err == nil {
+					tagValue = uint(vp)
+				}
+			}
+		case reflect.TypeOf(uint64(1)):
+			f = &cli.Uint64Flag{}
+			if tagString != "" {
+				if vp, err := strconv.ParseInt(tagString, 10, 64); err == nil {
+					tagValue = uint64(vp)
+				}
+			}
+		default:
+			continue
+		}
+
+		//
+
+		if v1.Ptr.Interface() != defaultValue {
+			if err := inflect.Set(f, "Value", v1.Ptr.Interface()); err != nil &&
+				err.Error() != "field does not exist" {
+				// TODO:
+			}
+		} else if tagString != "" {
+			if err := inflect.Set(f, "Value", tagValue); err != nil &&
+				err.Error() != "field does not exist" {
+				// TODO:
+			}
+		}
+
+		name, usage, envVar, hidden := b.extractSharedParts(commandName, k1, v1)
+		b.setSharedParts(f, name, usage, envVar, hidden)
+		if err := inflect.Set(f, "Destination", v1.Ptr.Addr().Interface()); err != nil {
+			// TODO:
+		}
+
+		*flags = append(*flags, f)
+	}
+}
+
+// Build builds the arguments/flags
+func (b *Argify) Build(app *cli.App, confPtr interface{}) error {
+	fields, err := inflectlab.GetFields(confPtr)
+	if err != nil {
+		return err
+	}
+
+	b.appName = app.Name
+	b.process("", fields, &app.Commands, &app.Flags)
+
+	return nil
+}
---
+++ example/main.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"gitlab.com/dc0d/tune"
+
+	"github.com/comail/colog"
+	"github.com/dc0d/argify"
+	"github.com/hashicorp/hcl"
+	"github.com/urfave/cli"
+)
+
+var (
+	cnf ConfPile
+
+	// DefaultConfNameHandler generates conf name,
+	// conf file name: conf-<app_name>.conf or app.conf
+	DefaultConfNameHandler = func() string {
+		fp := fmt.Sprintf("conf-%s.conf", DefaultAppNameHandler())
+		if _, err := os.Stat(fp); err != nil {
+			fp = "app.conf"
+		}
+		return fp
+	}
+
+	// DefaultAppNameHandler generates app name,
+	// default is filepath.Base(os.Args[0])
+	DefaultAppNameHandler = func() string {
+		return filepath.Base(os.Args[0])
+	}
+)
+
+// LoadHCL loads hcl conf files;
+// expected conf file name: DefaultConfNameHandler
+func LoadHCL(ptr interface{}, filePath ...string) error {
+	var fp string
+	if len(filePath) > 0 {
+		fp = filePath[0]
+	}
+	if fp == "" {
+		fp = DefaultConfNameHandler()
+	}
+	cn, err := ioutil.ReadFile(fp)
+	if err != nil {
+		return err
+	}
+	err = hcl.Unmarshal(cn, ptr)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type CouchDBInfo struct {
+	URL      string
+	User     string
+	Password string
+	DBName   string
+}
+
+// ConfPile sample conf struct
+type ConfPile struct {
+	Version1 string `name:"v1" usage:"--v1 0.0.1" hidden:"false" envvar:"V1" value:"0.0.1"`
+	Version2 string `name:"v2" usage:"--v2 0.0.1" hidden:"false" value:"0.0.1"`
+
+	FBool     bool
+	FDuration time.Duration
+	FFloat64  float64
+	FInt64    int64
+	FInt      int `value:"33"`
+	FString   string
+	FUint     uint   `value:"2"`
+	FUint64   uint64 `value:"66"`
+
+	App struct {
+		LogDir       string
+		PostponeExit time.Duration
+		Env          string
+		Version1     string
+	}
+
+	// one object for each database
+	PrimaryDB CouchDBInfo
+
+	Start struct {
+		Path     string `name:"path,p" usage:"-p ~/C" hidden:"false" value:"/tmp"`
+		Interval int
+	}
+}
+
+func init() {
+	colog.Register()
+	colog.SetFormatter(&colog.StdFormatter{Colors: true, Flag: log.Lshortfile})
+
+	if err := LoadHCL(&cnf); err != nil {
+		switch err.(type) {
+		case *os.PathError:
+			log.Println("warn:", err)
+		default:
+			panic(err)
+		}
+	}
+}
+
+func before(ctx *cli.Context) error {
+	return nil
+}
+
+func after(ctx *cli.Context) error {
+	return nil
+}
+
+func appInfo() *cli.App {
+	app := cli.NewApp()
+	app.Usage = "some app"
+	app.Version = "0.0.1"
+	app.Author = "dc0d"
+	app.Copyright = "dc0d"
+	app.Description = fmt.Sprintf("Built Time %v", time.Now())
+	app.Name = "gistcli"
+
+	app.Before = before
+	app.After = after
+
+	return app
+}
+
+func show(v interface{}) {
+	js, _ := json.MarshalIndent(v, "", "  ")
+	log.Printf("JSON:\n%s", js)
+}
+
+func appCommands(app *cli.App) {
+	{
+		app.Action = func(*cli.Context) error {
+			show(&cnf)
+			return nil
+		}
+	}
+
+	{
+		c := cli.Command{
+			Name:  `start`,
+			Usage: `starts feedwreq`,
+			Action: func(*cli.Context) error {
+				defer tune.Finit(cnf.App.PostponeExit)
+				log.Printf("%+v", cnf)
+				return nil
+			},
+		}
+		app.Commands = append(app.Commands, c)
+	}
+}
+
+func initApp() *cli.App {
+	app := appInfo()
+	appCommands(app)
+
+	argify.NewArgify().Build(app, &cnf)
+
+	return app
+}
+
+func main() {
+	app := initApp()
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatalln("error:", err)
+	}
+}
